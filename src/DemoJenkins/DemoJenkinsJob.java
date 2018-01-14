d8d74d3bbbd44299045907c624cce52b3e384976
aabhigaur863 <gaurabhi4u@gail.com>
2018-01-14 22:14:53 +0530

    added Jenkins first project

---
+++ src/DemoJenkins/DemoJenkinsJob.java
@@ -0,0 +1,13 @@
+package DemoJenkins;
+
+import org.testng.annotations.Test;
+
+public class DemoJenkinsJob {
+	
+	@Test
+	public void testJenkins()
+{
+System.out.println("Welcome To Jenkins");		
+}
+
+}
